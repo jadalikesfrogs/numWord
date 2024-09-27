3f7119db66568641a24bd6c513eee31e6cdf40c1
Jada Smith <jada.smith@lethpolytech.ca>
2024-09-27 08:36:43 -0600

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int getInt(int a, int b); // get int in range (a,b)
+string intWords(int a); // returns the word of the number
+
+int main() {
+	int number = 0;
+	int min = 1, max = 10;
+	
+	cout << "Enter a number: ";
+	
+	number = getInt(min, max);
+	cout << intWords(number);
+	return 0;
+}
+
+int getInt(int a, int b)
+{
+	cout << "getInt() -- stub" << endl;
+	return 1;
+}
+
+string intWords(int a)
+{
+
+
+	return string();
+}
